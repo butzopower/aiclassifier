df3c5c9307d8f1615502355e5711fde5a5f22a20
Brian Butz <bbutz@ris-spare-laptop.(none)>
2010-05-07 18:23:26 -0500

    kNN is working

--- Rakefile
+++ Rakefile
@@ -1,4 +1,5 @@
 require 'lib/naive_bayes'
+require 'lib/knn'
 
 namespace :nb do
   task :tictactoe do
@@ -42,8 +43,50 @@ namespace :nb do
     end
 
     puts "Accuracy: #{num_correct} / #{same_class_rows.size} (#{num_correct / (same_class_rows.size * 1.0)})"
-
   end
 end
 
+namespace :knn do
+  task :tictactoe do
+    knn = KNN.new
+    rows = Util.get_rows(:file => 'data/tic-tac-toe.data', :n => 500)
+    train_rows = rows.first(400)
+    train_classes = train_rows.transpose.last
+    train_rows = train_rows.transpose[0..-2].transpose
 
+    knn.train(train_rows, train_classes)
+
+    num_correct = 0
+    rows.last(100).each do |row|
+      r = row[0..-2]
+      c = row.last
+      classified = knn.classify(r)
+      puts "#{row.join(',')} - Classified as: #{classified.first}"
+      num_correct += 1 if c == classified.first
+    end
+
+    puts "Accuracy: #{num_correct} / 100 (#{num_correct / 100.0})"
+  end
+
+  task :vehicles do
+    knn = KNN.new
+    rows = Util.get_rows(:file => 'data/vehicles.csv', :n => 5000, :pre_process => true)
+    train_rows = rows.first(4000)
+    train_classes = train_rows.transpose.last
+    train_rows = train_rows.transpose[0..-2].transpose
+
+    knn.train(train_rows, train_classes)
+
+    num_correct = 0
+    same_class_rows = rows.last(1000).select{|x| x.last == "12FDEW6"}
+    same_class_rows.each do |row|
+      r = row[0..-2]
+      c = row.last
+      classified = knn.classify(r)
+      puts "#{row.join(',')} - Classified as: #{classified.first}"
+      num_correct += 1 if c == classified.first
+    end
+
+    puts "Accuracy: #{num_correct} / #{same_class_rows.size} (#{num_correct / (same_class_rows.size * 1.0)})"
+  end
+end
--- lib/knn.rb
+++ lib/knn.rb
@@ -1,8 +1,12 @@
 class KNN
   require 'lib/util'
-  require 'rubygems'
-  require 'ruby-debug'
   
+  # training for knn is normally small
+  # set rows and classes to variables
+  # since we are comparing categories with the function
+  #     catA - catB = sum_over_k( P(catA | k) - P(catB | k) )
+  # we can calculate sum_over_k( P(cat | k) to make classifying quicker
+  #
   def train(rows, classes)
     @rows = rows
     @classes = classes
@@ -25,24 +29,53 @@ class KNN
         end
       end
     end
-
   end
 
-  def classify(row)
+  # classify by getting the distance of the row we are trying to classify with
+  # the rows from the training set
+  # then take the k shortest distances associated with classes and weigh them 
+  # inversely proportional to their distances, sum up for all relevant classes
+  # and return the class with the highest value
+  
+  def classify(classifying_row, options = {})
+    k = options.delete(:k) || 10
     distances = []
-    @rows.each do |row|
-
+    @rows.each_with_index do |row, row_num|
+      distance = Array.new(row.size)
+      row.each_with_index do |elem, elem_num|
+        if elem.is_a?(String) || elem.is_a?(CSV::Cell)  # if the element is categorical
+          # distance = probability of category for row we are classifying minus
+          # the probability of the category of the row we are measuring the
+          # distance against
+          distance[elem_num] = @cat_probs[elem_num][classifying_row[elem_num]] - @cat_probs[elem_num][elem] 
+        else
+          # if not categorical variable, then just subtract the value of the
+          # classifying variable with the variable we are measuring against
+          distance[elem_num] = classifying_row[elem_num].to_i - elem.to_i
+        end
+      end
+      d = sum(distance.map{|x| x*x})  # dot(distance, distance)
+      distances << [d, @classes[row_num]]
     end
+
+    distances.sort.first(k).inject(Hash.new(0)) do |hash, distance|
+      hash[distance.last] += 1.0 / distance.first # weigh the class at an inverse to it's distance
+                                                  # if distance is 0, weight will be infinity
+      hash
+    end.to_a.sort_by(&:last).last.first           # sort by weight, return class with highest weight
   end
 
+  # ruby doesn't have a sum method, but this is pretty simple
   def sum(array)
     array.inject(0){|sum, n| sum + n}
   end
 
+  # use in interactive ruby to do a quick test of data set
   def test
-    rows = Util.get_rows(:file => 'data/vehicles.csv', :n => 1000, :pre_process => true)
+    rows = Util.get_rows(:file => 'data/vehicles.csv', :n => 3000, :pre_process => true)
     classes = rows.transpose.last
     rows = rows.transpose[0..-2].transpose
-    train(rows, classes)
+    train(rows.first(2000), classes.first(2000))
+    "#{classes.last} classified as #{classify(rows.last)}"
   end
 end
